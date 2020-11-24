8437c6087bae17e62fcfa87cb6d4a1a5c143c796
wateryloo <zhang_jia_shu@qq.com>
2020-11-24 23:04:28 +0800

    A Very Weak Version Improvement is necessary.

--- IMP.py
+++ IMP.py
@@ -1,26 +1,25 @@
 import argparse
-# import numpy as np
-import time
 import random
-# import multiprocessing as mp
-# import sys
-import os
 import math
 
 
-def log_comb(n, k):
-    res = 0
-    while n > k:
+def log_comb(n, k) -> float:
+    res = 0.0
+    i = 0
+    while i < k:
         res += math.log(n)
         n -= 1
+        i += 1
     while k > 1:
-        res -= math.log()
+        res -= math.log(k)
+        k -= 1
+    return res
 
 
 def IMM(out_graph, in_graph, n, k, epsilon, l, model):
     l = l * (1 + 0.6931471805599453 / math.log(n))
     R = sampling(out_graph, in_graph, n, k, epsilon, l, model)
-    print('sampling done')
+    # print('end sampling')
     S_k = node_select(R, k)
     return S_k
 
@@ -36,55 +35,66 @@ def F_R(R, S):
 
 def node_select(R, k):
     S_k = set()
-    all_union = set()
-    for rr in R:
-        all_union = all_union.union(rr)
-    for i in range(1, k + 1):
-        max_ratio = 0.0
-        v_that_max = all_union.pop()
-        all_union.add(v_that_max)
-        for v in all_union:
-            v_s = set()
-            v_s.add(v)
-            S_k_union_v = S_k.union(v_s)
-            ratio = F_R(R, S_k_union_v)
-            if ratio > max_ratio:
-                max_ratio = ratio
-                v_that_max = v
-        S_k.add(v_that_max)
-        all_union.discard(v_that_max)
+    vertex_list_map = {}
+    vertex_frequency_map = {}
+    i: int = 0
+    R_length = len(R)
+    while i < R_length:
+        for vertex in R[i]:
+            if vertex not in vertex_list_map:
+                vertex_list_map[vertex] = []
+            vertex_list_map[vertex].append(i)
+        i += 1
+    for vertex in vertex_list_map:
+        vertex_frequency_map[vertex] = len(vertex_list_map[vertex])
+    for i in range(0, k):
+        v = None
+        max_frequency = -1
+        for vertex in vertex_frequency_map:
+            frequency = vertex_frequency_map[vertex]
+            if frequency > max_frequency:
+                v = vertex
+                max_frequency = frequency
+        S_k.add(v)
+        for rr_id in vertex_list_map[v]:
+            for vertex in R[rr_id]:
+                vertex_frequency_map[vertex] -= 1
     return S_k
 
 
 def generate_rr(out_graph, in_graph, v, model):
-    # print('generate_rr', v)
+    activated = set()
     activate_set = set()
     activate_set.add(v)
-    activated = set()
+    activated.add(v)
     if model == 'IC':
         while len(activate_set) > 0:
-            # print(len(activate_set))
-            act = activate_set.pop()
-            activated.add(act)
-            for (source, dest, weight) in in_graph[act]:
-                if weight >= random.random():
-                    if source not in activated:
-                        activate_set.add(source)
-                        # print(activate_set)
-        return activated
+            new_activate_set = set()
+            for act in activate_set:
+                for (source, dest, weight) in in_graph[act]:
+                    if source in activated:
+                        continue
+                    if weight >= random.random():
+                        new_activate_set.add(source)
+                        activated.add(source)
+            activate_set = new_activate_set
     else:
         while len(activate_set) > 0:
-            act = activate_set.pop()
-            activated.add(act)
-            rand_map = {}
-            sum = 0.0
-            for (source, dest, weight) in in_graph[act]:
-                rand_map[source] = random.random()
-                sum += rand_map[source]
-            for (source, dest, weight) in in_graph[act]:
-                if weight >= (rand_map[source] / sum):
-                    activate_set.add(source)
-        return activated
+            new_activate_set = set()
+            for act in activate_set:
+                rand_map = {}
+                sum = 0.0
+                for (source, dest, weight) in in_graph[act]:
+                    rand_map[source] = random.random()
+                    sum += rand_map[source]
+                for (source, dest, weight) in in_graph[act]:
+                    if source in activated:
+                        continue
+                    if weight >= (rand_map[source] / sum):
+                        new_activate_set.add(source)
+                        activated.add(source)
+            activate_set = new_activate_set
+    return activated
 
 
 def comb(n, k):
@@ -98,12 +108,12 @@ def comb(n, k):
 
 
 def lambda_prime(n, k, l, epsilon_prime):
-    print('epsilon_prime', epsilon_prime)
+    # print('epsilon_prime', epsilon_prime)
     numerator = (2 + 2 * epsilon_prime / 3) * (
-            math.log(comb(n, k)) + l * math.log(n) + math.log(math.log2(n))) * n
+            log_comb(n, k) + l * math.log(n) + math.log(math.log2(n))) * n
     denominator = epsilon_prime ** 2
-    print('numerator', numerator)
-    print('denominator', denominator)
+    # print('numerator', numerator)
+    # print('denominator', denominator)
     return numerator / denominator
 
 
@@ -112,25 +122,29 @@ def sampling(out_graph, in_graph, n, k, epsilon, l, model):
     R = []
     epsilon_prime = epsilon * 1.41421356237
     i = 1
-    while i < math.log2(n):
+    upper = math.log2(n)
+    while i < upper:
+        # print('sampling', i, math.log2(n))
         x = n / math.pow(2, i)
-        print('n', n)
-        print('x', x)
         theta_i = lambda_prime(n, k, l, epsilon_prime) / x
-        print('theta_i', theta_i)
         while len(R) < theta_i:
+            # print('sampling: len(R)', len(R), 'theta_i:', theta_i)
             v = random.randint(1, n + 1)
             RR = generate_rr(out_graph, in_graph, v, model)
             R.append(RR)
+        # print('Enter node_select')
         S_i = node_select(R, k)
-        if n * F_R(R, S_i) >= (1 + epsilon_prime) * x:
-            LB = n * F_R(R, S_i) / (1 + epsilon_prime)
+        # print('Exit node select')
+
+        cover_ratio = F_R(R, S_i)
+        if n * cover_ratio >= (1 + epsilon_prime) * x:
+            LB = n * cover_ratio / (1 + epsilon_prime)
             break
         i += 1
 
     alpha = math.sqrt(l * math.log(n) + 0.6931471805599453)
     beta = math.sqrt(
-        (1 - 1 / math.e) * (math.log(comb(n, k)) + l * math.log(n) + 0.6931471805599453))
+        (1 - 1 / math.e) * (log_comb(n, k) + l * math.log(n) + 0.6931471805599453))
     lambda_star = 2 * n * (((1 - 1 / math.e) * alpha + beta) ** 2) * (epsilon ** -2)
     theta = lambda_star / LB
     while len(R) < theta:
@@ -140,9 +154,8 @@ def sampling(out_graph, in_graph, n, k, epsilon, l, model):
     return R
 
 
-# -i C:\Users\Jiash\Desktop\IMP\DatasetOnTestPlatform\network.txt -k 5 -m IC -t 60
+# -i C:\Users\Jiash\Desktop\IMP\DatasetOnTestPlatform\NetHEPT.txt -k 5 -m IC -t 60
 if __name__ == '__main__':
-    start = time.time()
     l = 1
     epsilon = 0.5
 
@@ -153,6 +166,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--time', type=int)
 
     args = parser.parse_args()
+    # print(args)
     network = args.network
     seedCount = args.seedCount
     model = args.model
@@ -181,9 +195,6 @@ if __name__ == '__main__':
         weight = float(tokens[2])
         outGraph[source].append((source, dest, weight))
         inGraph[dest].append((source, dest, weight))
-
-    end = time.time()
-    print('read graph: ', end - start)
 
     epsilon = 0.5
     l = 1
--- main.py
+++ main.py
@@ -4,4 +4,11 @@ import random
 import math
 
 if __name__ == '__main__':
-    print(math.log(2))
+    d = {1: 2, 2: 3, 3: 1}
+    del[d[1]]
+    l = sorted(d.items(), key=lambda x:x[1])
+    print(l)
+    l1 = [1, 2, 3]
+    l2 = [4, 5, 6]
+    l1 += l2
+    print(l1)
